0406731792e1f19a46e04197984090f9ee8acce0
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-27 11:11:45 +0100

    updated path aliases

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { createTRPCRouter } from "y/server/api/trpc";
-import { exampleRouter } from "y/server/api/routers/example";
+import { createTRPCRouter } from "~/server/api/trpc";
+import { packageRouter } from "./routers/packages";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "y/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  package: packageRouter,
 });
 
 // export type definition of API
