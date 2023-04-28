3933045a656d0ca6e65a84ef267e9289faaffe78
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-28 02:10:17 +0100

    added copy button also configured SVGR

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,31 @@
+import { FC, useState } from "react";
+import Clipboard from "~/icons/clipboard.svg";
+
+interface props {
+  text: string;
+}
+
+let CopyButton: FC<props> = ({ text }) => {
+  let [recent, setRecent] = useState(false);
+
+  return (
+    <button
+      data-tip={recent ? "Copied!" : "Copy?"}
+      className={`tooltip ${
+        recent ? "tooltip-open" : ""
+      } p-1 transition-transform active:-translate-y-2 `}
+      onClick={() => {
+        navigator.clipboard.writeText(text);
+        setRecent(true);
+
+        setTimeout(() => {
+          setRecent(false);
+        }, 3000);
+      }}
+    >
+      <Clipboard width="15" className="inline -translate-y-1 " />
+    </button>
+  );
+};
+
+export default CopyButton;
