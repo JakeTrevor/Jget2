6c915dd75f1716439e8cb910f6981d313bbece0d
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-28 19:00:14 +0100

    fixed baseline in header
    thanks @petro

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,18 @@ import Link from "next/link";
 
 export default () => {
   return (
-    <header className="navbar sticky top-0 bg-base-100 align-baseline">
-      <Link href="/" className="btn-ghost btn font-title text-4xl normal-case">
-        <h1>JGET</h1>
+    <header className="navbar sticky top-0 bg-base-100">
+      <Link
+        href="/"
+        className="debug btn-ghost btn flex items-end pb-1 font-title  normal-case"
+      >
+        <h1 className="text-4xl">JGET</h1>
       </Link>
       <Link
         href="/packages"
-        className="btn-ghost btn font-title  text-xl normal-case"
+        className="btn-ghost btn flex items-end pb-0.5 font-title normal-case"
       >
-        <h3>explore</h3>
+        <h3 className="text-xl">explore</h3>
       </Link>
     </header>
   );
