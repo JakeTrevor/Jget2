dcccdc1ab6f4f7e795c2a9ca43b6a0c3f234923a
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-05-01 21:42:00 +0100

    file list styling also added back button

--- src/components/fileBrowser/FileList.tsx
+++ src/components/fileBrowser/FileList.tsx
@@ -24,28 +24,31 @@ let FileList: FC<props> = ({ package_name, data }) => {
   });
 
   return (
-    <table className="table w-full">
-      <thead>
-        <th>Type</th>
-        <th>File Name</th>
-      </thead>
-      <tbody>
-        {keys.map((key) => (
-          <tr>
-            <th className="w-1">
-              {typeof data[key] === "string" ? (
-                <File width={15} />
-              ) : (
-                <Folder width={15} />
-              )}
-            </th>
-            <td>
-              <Link href={`/package/${package_name}/${key}`}>{key}</Link>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <section className="grid w-full grid-cols-3 gap-y-2 sm:grid-cols-7 md:grid-cols-6 lg:grid-cols-9 xl:grid-cols-12">
+      <p className="col-span-1 mb-1 bg-zinc-800 pb-1 pr-2 text-end font-title">
+        Type
+      </p>
+      <p className="col-span-2 mb-1 bg-zinc-800 pb-1 pl-2 font-title sm:col-span-6 md:col-span-5 lg:col-span-8 xl:col-span-11">
+        File Name
+      </p>
+      {keys.map((key) => (
+        <>
+          <div className="col-span-1 flex items-center justify-end pr-2">
+            {typeof data[key] === "string" ? (
+              <File className="text-white" width={15} />
+            ) : (
+              <Folder width={15} />
+            )}
+          </div>
+          <Link
+            className="link-hover link col-span-2 pl-2 sm:col-span-6 md:col-span-5 lg:col-span-8 xl:col-span-11"
+            href={`/package/${package_name}/${key}`}
+          >
+            {key}
+          </Link>
+        </>
+      ))}
+    </section>
   );
 };
 
--- src/components/fileBrowser/index.tsx
+++ src/components/fileBrowser/index.tsx
@@ -3,6 +3,7 @@ import FileDisplay from "./FileDisplay";
 import FileList from "./FileList";
 import Link from "next/link";
 
+import Back from "~/icons/back.svg";
 interface props {
   data: Directory;
   pointer: string[];
@@ -25,24 +26,50 @@ function getDir(data: Directory, pointer: string[]) {
 let FileBrowser: FC<props> = ({ package_name, data, pointer }) => {
   let result = getDir(data, pointer);
 
-  let crumbs = [package_name, ...pointer];
+  let backDest =
+    pointer.length > 0
+      ? `/package/${package_name}/${pointer.slice(0, -1).join("/")}`
+      : `/package/${package_name}`;
+
+  let crumbs = [...pointer];
 
   return (
-    <section className="w-3/4">
-      <div className="breadcrumbs text-sm">
-        <ul>
-          {crumbs.map((e, i, arr) => {
-            if (i === arr.length - 1) return <li>{e}</li>;
-            let path = arr.slice(0, i + 1).join("/");
+    <section className="mockup-code mb-20 min-h-[50vh] w-3/4 bg-secondary p-5 shadow-xl">
+      <div className="flex flex-row justify-between">
+        <div className="breadcrumbs text-sm">
+          <ul className="font-mono underline-offset-4">
+            <li>
+              {pointer.length > 0 ? (
+                <Link href={`/package/${package_name}`}>/</Link>
+              ) : (
+                <>/</>
+              )}
+            </li>
+            {crumbs.map((e, i, arr) => {
+              if (i === arr.length - 1) return <li>{e}</li>;
+              let path = arr.slice(0, i + 1).join("/");
 
-            return (
-              <li>
-                <Link href={`/package/${path}`}>{e}</Link>
-              </li>
-            );
-          })}
-        </ul>
+              return (
+                <li>
+                  <Link href={`/package/${package_name}/${path}`}>{e}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <Link
+          className={`transition-all ${
+            pointer.length > 0
+              ? "tooltip hover:scale-110"
+              : "cursor-default text-zinc-500"
+          }`}
+          data-tip="Back"
+          href={backDest}
+        >
+          <Back width={15} />
+        </Link>
       </div>
+      <div className="divider my-1 -mb-3 before:bg-zinc-700 after:bg-zinc-700" />
       {typeof result === "string" ? (
         <FileDisplay data={result} />
       ) : (
