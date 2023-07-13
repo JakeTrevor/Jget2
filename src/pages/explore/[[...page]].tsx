d408d9b80898d9c11b7faaf8ef66039d02d1e4a5
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-07-13 21:54:52 +0100

    refactored explore page layout with grid

--- src/pages/explore/[[...page]].tsx
+++ src/pages/explore/[[...page]].tsx
@@ -7,16 +7,12 @@ import PackageListing from "~/components/package/PackageListing";
 import { api } from "~/utils/api";
 
 const Packages: NextPage = () => {
-  let { page: rawPage } = useRouter().query;
-
-  rawPage ||= "1";
+  let rawPage = useRouter().query.page || "1";
 
   let page: number = 1;
-  if (typeof rawPage === "string") {
-    page = parseInt(rawPage) || 1;
-  } else {
-    page = parseInt(rawPage[0] || "1") || 1;
-  }
+
+  if (typeof rawPage === "string") page = parseInt(rawPage) || 1;
+  else page = parseInt(rawPage[0] || "1") || 1;
 
   let { data, isLoading } = api.package.getList.useQuery({ page });
 
@@ -30,21 +26,24 @@ const Packages: NextPage = () => {
         <meta name="description" content="JGET Package Manager" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex h-[93vh] w-full flex-col items-center justify-around bg-base-200">
+      <main className="grid h-[93vh] w-full grid-cols-4 bg-base-200">
         {isLoading ? (
           <Loading />
         ) : (
           <>
-            <ul className="menu rounded-box h-[52vh] w-1/2 bg-base-100 p-2">
-              {packages.map((pkg) => (
-                <PackageListing data={pkg} />
-              ))}
-            </ul>
-            <PageControls
-              page={page}
-              num_pages={num_pages}
-              base={"/explore/"}
-            />
+            <div id="FilterControls" />
+            <div className="col-span-2 flex flex-col items-center justify-around">
+              <ul className="menu rounded-box h-[52vh] w-full bg-base-100 p-2">
+                {packages.map((pkg) => (
+                  <PackageListing data={pkg} />
+                ))}
+              </ul>
+              <PageControls
+                page={page}
+                num_pages={num_pages}
+                base={"/explore/"}
+              />
+            </div>
           </>
         )}
       </main>
