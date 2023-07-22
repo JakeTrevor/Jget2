4d2e57c2b6e47450cd74b24ec1c14d9e26fc44aa
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-07-22 18:57:44 +0100

    fixed package listing on laptop viewscreen

--- src/components/package/PackageListing.tsx
+++ src/components/package/PackageListing.tsx
@@ -10,14 +10,15 @@ let PackageListing: FC<props> = ({
   data: { name, downloads, createdAt, updatedAt },
 }) => {
   return (
-    <li className="h-[10vh]">
-      <Link href={`/package/${name}/`} className="grid grid-cols-3">
-        <h2 className="col-span-3 text-xl font-bold">{name}</h2>
-        <p>created: {createdAt.toLocaleDateString()}</p>
-        <p>updated: {updatedAt.toLocaleDateString()}</p>
-        <p className="text-right">{downloads} downloads</p>
-      </Link>
-    </li>
+    <Link
+      href={`/package/${name}/`}
+      className="grid h-[12vh] grid-cols-3 rounded-sm px-2 py-1 transition-all duration-300 hover:scale-105 hover:bg-base-300"
+    >
+      <h2 className="col-span-3 text-lg font-bold">{name}</h2>
+      <p>created: {createdAt.toLocaleDateString()}</p>
+      <p>updated: {updatedAt.toLocaleDateString()}</p>
+      <p className="text-right">{downloads} downloads</p>
+    </Link>
   );
 };
 
