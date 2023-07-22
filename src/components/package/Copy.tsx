656ae9d96dbb2f25a91f8d27e71fee7727e9e6d1
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-07-22 19:32:49 +0100

    fixed colour for copy button label

--- src/components/package/Copy.tsx
+++ src/components/package/Copy.tsx
@@ -8,9 +8,7 @@ interface props {
 let component: FC<props> = ({ package_name }) => {
   return (
     <div>
-      <h4 className="font-semibold italic text-accent">
-        Install this package:
-      </h4>
+      <h4 className="font-semibold italic">Install this package:</h4>
       <pre className="rounded-md bg-code p-3 text-emerald-500">
         $ <span className="text-white">jget get {package_name} </span>
         <CopyButton text={`jget get ${package_name}`} />
