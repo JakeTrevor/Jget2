ebda9905e8a8566c012f309690c81f6510999f9c
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-07-22 23:15:18 +0100

    added docs on authentication

--- src/pages/profile/tokens.tsx
+++ src/pages/profile/tokens.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import Token from "~/components/token";
 
@@ -6,7 +7,16 @@ const Profile: NextPage = () => {
   let router = useRouter();
   return (
     <main className="flex h-[93vh] flex-col items-center bg-base-200 p-10">
-      <h2 className="self-start text-2xl font-bold">Your access tokens</h2>
+      <h2 className="self-start text-2xl font-bold">
+        Your{" "}
+        <Link
+          href="/docs/authentication"
+          className="tooltip underline decoration-dotted"
+          data-tip="What is this?"
+        >
+          access tokens
+        </Link>
+      </h2>
       <div className="divider" />
       <div className="flex w-3/4 flex-col rounded-md bg-white p-4">
         <Token />
