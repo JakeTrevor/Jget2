d2def6ac7d2a69f6df6381b4111711c8a88fd96d
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-08-30 18:44:37 +0100

    basic scaffold for editor

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,13 @@
 import type { FC, ReactNode } from "react";
 import Header from "./header";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 let Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  const router = useRouter();
+
+  if (router.pathname === "/edit") return <>{children}</>;
+
   return (
     <>
       <Head>
