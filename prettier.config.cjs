a60122ef88c9d339cb9b534f77f616ff48e2bade
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-12-26 21:46:15 +0000

    starting re-writing app

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [
-    require.resolve("prettier-plugin-tailwindcss"),
-    require.resolve("prettier-plugin-prisma"),
-  ],
-};
-
-module.exports = config;
