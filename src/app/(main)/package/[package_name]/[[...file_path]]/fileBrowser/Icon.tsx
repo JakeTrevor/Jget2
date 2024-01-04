75dcbd56062233f8760441fe1692742bfa9f0967
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-01-04 01:19:56 +0000

    fixed icons for file types

--- src/app/(main)/package/[package_name]/[[...file_path]]/fileBrowser/Icon.tsx
+++ src/app/(main)/package/[package_name]/[[...file_path]]/fileBrowser/Icon.tsx
@@ -1,13 +1,13 @@
-import { Folder, File, FileBox } from "lucide-react";
-// import LuaFile from "~/icons/luaFile.svg";
-// import MarkdownFile from "~/icons/markdownFile.svg";
+import { File, FileBox, Folder } from "lucide-react";
+import LuaFile from "~/icons/luaFile.svg";
+import MarkdownFile from "~/icons/markdownFile.svg";
 
 export const Icon = ({ item, data }: { item: string; data: Directory }) => {
   if (typeof data[item] !== "string") return <Folder size={15} />;
 
-  if (item.endsWith(".lua")) return <>lua</>;
+  if (item.endsWith(".lua")) return <LuaFile width={15} />;
 
-  if (item.endsWith(".md")) return <>md</>;
+  if (item.endsWith(".md")) return <MarkdownFile width={15} />;
 
   if (item.toLowerCase() === "dependencies.txt") return <FileBox size={15} />;
 
