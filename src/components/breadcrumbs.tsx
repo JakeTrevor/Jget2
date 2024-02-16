c776c4631fb7ce918f859ad3d9b310906775da65
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-02-16 21:04:41 +0000

    refactored breadcrumbs component

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -19,18 +19,12 @@ export const Breadcrumbs = ({
         <Comp href={base}>{baseIcon}</Comp>
       </li>
       {path.map((e, i, arr) => {
-        if (i === arr.length - 1)
-          return (
-            <li key={e}>
-              <ChevronRight className="inline" size={15} />
-              {e}
-            </li>
-          );
+        const Comp = i === arr.length - 1 ? Link : Fragment;
 
         return (
           <li key={e}>
             <ChevronRight className="inline" size={15} />
-            <Link href={base + arr.slice(0, i + 1).join("/")}>{e}</Link>
+            <Comp href={e}>{i === 0 ? baseIcon : e}</Comp>
           </li>
         );
       })}
