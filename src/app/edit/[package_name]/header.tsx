4cb74e3a86a32f334e5e69e9589b7ba2bd93ac40
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-04-05 19:20:32 +0100

    cleaned up header

--- src/app/edit/[package_name]/header.tsx
+++ src/app/edit/[package_name]/header.tsx
@@ -1,33 +1,16 @@
 import Link from "next/link";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarTrigger,
-} from "~/components/ui/menubar";
-import { ThemeToggleSubmenu } from "~/components/ui/theme";
+import { ThemeToggle } from "~/components/ui/theme";
 import { SaveButton } from "./SaveButton";
+import { Button } from "~/components/ui/button";
 
 export function Header({ packageName }: { packageName: string }) {
   return (
-    <header className="h-[5vh] w-full bg-red-100 text-foreground">
-      <Menubar className="rounded-none">
-        <MenubarMenu>
-          <MenubarTrigger className="h-12 font-title text-xl">
-            JGET
-          </MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>
-              <Link href="/">Home</Link>
-            </MenubarItem>
-            <ThemeToggleSubmenu />
-            <MenubarItem>
-              <SaveButton packageName={packageName} />
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
+    <header className="flex h-[5dvh] w-full flex-row items-baseline gap-4 p-1 text-foreground shadow-lg">
+      <Button variant="ghost" className="font-title text-xl" asChild>
+        <Link href="/">JGET</Link>
+      </Button>
+      <SaveButton packageName={packageName} />
+      <ThemeToggle />
     </header>
   );
 }
